96fcf798276ee67b9c4ff41126dc8644d36a46c9
Elias O'Donnell <50235043+Elias-Odonnell@users.noreply.github.com>
2022-01-08 22:19:11 +0000

    Code clean up and added long press for adjusting brightness

--- code.py
+++ code.py
@@ -1,3 +1,8 @@
+import time
+import board
+import neopixel
+import digitalio
+
 import adafruit_ble
 from adafruit_ble.advertising import Advertisement
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
@@ -7,40 +12,41 @@ from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 from adafruit_hid.keycode import Keycode
 
-import time
-import board
-import digitalio
-import neopixel
+#  setup for the onboard neopixels
+pixel_pin = board.NEOPIXEL
+num_pixels = 10
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.05, auto_write=False)
 
-pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=0.05)
+#  variables for colours, add here any extra colours
 RED = (255, 0, 0)
-YELLOW = (255, 150, 0)
 GREEN = (0, 255, 0)
-CYAN = (0, 255, 255)
-BLUE = (0, 0, 255)
-PURPLE = (180, 0, 255)
 WHITE = (255, 255, 255)
 OFF = (0, 0, 0)
 
+#  set up the internal buttons and switch
+# (the buttons are pull-down and the switch is up)
 buttonA = digitalio.DigitalInOut(board.BUTTON_A)
 buttonA.switch_to_input(pull=digitalio.Pull.DOWN)
 
 buttonB = digitalio.DigitalInOut(board.BUTTON_B)
 buttonB.switch_to_input(pull=digitalio.Pull.DOWN)
 
-pressed = time.monotonic()
+switch = digitalio.DigitalInOut(board.SLIDE_SWITCH)
+switch.direction = digitalio.Direction.INPUT
+switch.pull = digitalio.Pull.UP
 
-ledCounterAudio = 1
-ledCounterVideo = 1
-
+#  HID keyboard input setup
 hid = HIDService()
+keyboard = Keyboard(hid.devices)
+keyboard_layout = KeyboardLayoutUS(keyboard)
 
+# BLUETOOTH SETUP
 device_info = DeviceInfoService(software_revision=adafruit_ble.__version__,
                                 manufacturer="Adafruit Industries")
 advertisement = ProvideServicesAdvertisement(hid)
 advertisement.appearance = 961
 scan_response = Advertisement()
-scan_response.complete_name = "Prototype V1.0"
+scan_response.complete_name = "PROTOTYPE V1.1"  # change name here
 
 ble = adafruit_ble.BLERadio()
 if not ble.connected:
@@ -49,58 +55,138 @@ if not ble.connected:
 else:
     print("already connected")
     print(ble.connections)
-    pixels.fill(RED)
-    pixels.show()
 
-k = Keyboard(hid.devices)
-kl = KeyboardLayoutUS(k)
+#  state machines
+#  for button debouncing
+buttonA_pressed = False
+buttonB_pressed = False
+longPressA = False
+longPressB = False
+#  default mute states
+microphone_mute = True
+video_off = True
+#  time.monotonic() tracker for debouncing and long press
+clock = time.monotonic()
+
+
 while True:
     while not ble.connected:
         pass
     print("Start typing:")
 
     while ble.connected:
-        if time.monotonic() - pressed < 0.2:
-            continue
-        if buttonA.value:
-            print("ButtonA")  # button is pushed
-            k.send(Keycode.ALT, Keycode.A)
-            ledCounterAudio += 1
-            if ledCounterAudio > 2:
-                ledCounterAudio = 1
+        #  button debouncing
+        if not buttonA.value and buttonA_pressed:
+            buttonA_pressed = False
+        if not buttonB.value and buttonB_pressed:
+            buttonB_pressed = False
+        #  colour of the pixels based on different states.
+        if microphone_mute:
+            if switch.value:
+                pixels[5] = OFF
+                pixels[6] = OFF
+                pixels[7] = RED
+                pixels[8] = OFF
+                pixels[9] = OFF
+                pixels.show()
+            if not switch.value:
+                pixels[5] = WHITE
+                pixels[6] = WHITE
+                pixels[7] = RED
+                pixels[8] = WHITE
+                pixels[9] = WHITE
+                pixels.show()
 
-            if ledCounterAudio == 1:
-                pixels[0] = GREEN
-                pixels[1] = GREEN
-                pixels[2] = YELLOW
-                pixels[3] = GREEN
-                pixels[4] = GREEN
-            if ledCounterAudio == 2:
-                pixels[0] = RED
-                pixels[1] = RED
-                pixels[2] = BLUE
-                pixels[3] = RED
-                pixels[4] = RED
-            pixels.show()
-            pressed = time.monotonic()
+        if video_off:
+            if switch.value:
+                pixels[0] = OFF
+                pixels[1] = OFF
+                pixels[2] = RED
+                pixels[3] = OFF
+                pixels[4] = OFF
+                pixels.show()
+            if not switch.value:
+                pixels[0] = WHITE
+                pixels[1] = WHITE
+                pixels[2] = RED
+                pixels[3] = WHITE
+                pixels[4] = WHITE
+                pixels.show()
 
-        if buttonB.value:
-            print("ButtonB")  # button is pushed
-            k.send(Keycode.ALT, Keycode.V)
-            ledCounterVideo += 1
-            if ledCounterVideo > 2:
-                ledCounterVideo = 1
-            if ledCounterVideo == 1:
-                pixels[5] = GREEN
-                pixels[6] = GREEN
-                pixels[7] = CYAN
-                pixels[8] = GREEN
-                pixels[9] = GREEN
-            if ledCounterVideo == 2:
-                pixels[5] = RED
-                pixels[6] = RED
-                pixels[7] = PURPLE
-                pixels[8] = RED
-                pixels[9] = RED
-            pixels.show()
-            pressed = time.monotonic()
+        if not microphone_mute:
+            if switch.value:
+                pixels[5] = OFF
+                pixels[6] = OFF
+                pixels[7] = GREEN
+                pixels[8] = OFF
+                pixels[9] = OFF
+                pixels.show()
+            if not switch.value:
+                pixels[5] = WHITE
+                pixels[6] = WHITE
+                pixels[7] = GREEN
+                pixels[8] = WHITE
+                pixels[9] = WHITE
+                pixels.show()
+
+        if not video_off:
+            if switch.value:
+                pixels[0] = OFF
+                pixels[1] = OFF
+                pixels[2] = GREEN
+                pixels[3] = OFF
+                pixels[4] = OFF
+                pixels.show()
+            if not switch.value:
+                pixels[0] = WHITE
+                pixels[1] = WHITE
+                pixels[2] = GREEN
+                pixels[3] = WHITE
+                pixels[4] = WHITE
+                pixels.show()
+
+        #  if you press button a
+        if (buttonA.value and not buttonA_pressed):
+            buttonA_pressed = True
+            clock = time.monotonic()  # start counting the time
+            time.sleep(0.3)  # change here for responsiveness of the click
+            if buttonA.value and buttonA_pressed:
+                print("Long press A")
+                while buttonA.value and buttonA_pressed:
+                    longPressA = True
+                    pixels.brightness += 0.0025
+                    pixels.show()
+            else:
+                if video_off:
+                    print("buttonA")
+                    video_off = False
+                    longPressA = False
+                elif not video_off:
+                    print("buttonA")
+                    video_off = True
+                    longPressA = False
+                keyboard.send(Keycode.ALT, Keycode.V)
+
+        #  if you press button B
+        if (buttonB.value and not buttonB_pressed):
+            buttonB_pressed = True
+            clock = time.monotonic()
+            time.sleep(0.3)
+            if buttonB.value and buttonB_pressed:
+                print("Long press B")
+                while buttonB.value and buttonB_pressed:
+                    longPressB = True
+                    pixels.brightness -= 0.0025
+                    pixels.show()
+            else:
+                if microphone_mute:
+                    print("buttonB")
+                    longPressB = True
+                    microphone_mute = False
+                    longPressB = False
+                elif not microphone_mute:
+                    print("buttonB")
+                    microphone_mute = True
+                    longPressB = False
+                keyboard.send(Keycode.ALT, Keycode.A)
+
